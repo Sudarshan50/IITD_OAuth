2f389a5a43489a8d0e24dd43110eff60db841f91
Sudarshan <105160284+Sudarshan50@users.noreply.github.com>
2025-01-16 17:59:40 +0530

    Refactor:Disable imp..

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,7 @@ import { adminMiddleWare } from "../middleware/adminMiddleWare.js";
 const adminRouter = e.Router();
 
 adminRouter.post("/signin", admin.signIn);
-adminRouter.post("/signup", admin.signUp);
+// adminRouter.post("/signup", admin.signUp);
 adminRouter.post("/register", adminMiddleWare, admin.registerClient);
 adminRouter.get("/clients", adminMiddleWare, admin.getAllClients);
 adminRouter.get("/client/:client_id", adminMiddleWare, admin.getClientById);
